ee58246a4d4e7fe6fa35df1334334f2670d8d552
Yuniesky Suarez <yuniesky@mobai.bio>
2024-10-24 13:41:59 +0200

    refactor login form

--- src/components/dialogs/LoginDialog.tsx
+++ src/components/dialogs/LoginDialog.tsx
@@ -8,90 +8,93 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
 import { Input } from "../input/Input.tsx";
-import { Box, Grid2, Paper } from "@mui/material";
+import { Box, Grid2, Paper, TextField } from "@mui/material";
 import { DataGrid, GridColDef, GridRowId, GridRowsProp } from '@mui/x-data-grid';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { GridActionsCellItem, } from '@mui/x-data-grid';
-
+import "./login-dialog.scss"
+import "./../../styles/variables.scss"
 
 export const LoginDialog = (props) => {
 
-  const [email, setEmail] = useState('');
-  const [isValid, setIsValid] = useState(true);
+    const [email, setEmail] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
-  const [emailInputState, setEmailInputState] = useState('default-input')
+    const [emailInputState, setEmailInputState] = useState('default-input')
 
-  const [password, setPassword] = useState('')
-  const [passwordInputState, setPasswordInputState] = useState('default-input')
+    const [password, setPassword] = useState('')
+    const [passwordInputState, setPasswordInputState] = useState('default-input')
 
-  const [emailErrorMsg, setEmailErrorMsg] = useState('')
-  const [passwordErrorMsg, setPasswordErrorMsg] = useState('')
+    const [emailErrorMsg, setEmailErrorMsg] = useState('')
+    const [passwordErrorMsg, setPasswordErrorMsg] = useState('')
 
-  const navigate = useNavigate()
+    const navigate = useNavigate()
 
-  const handleEmailChange = (e: string) => {
-    setEmail(e)
-    if (e === '') {
-      setEmailInputState('default-input')
-    } else {
-      setIsValid(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e));
-      if (isValid) {
-        setEmailInputState('valid-input')
-      } else {
-        setEmailInputState('invalid-input')
-      }
-    }
-  };
-
-
-  const handlePasswordChange = (e: string) => {
-    setPassword(e)
-    if (e !== '') {
-      setPasswordInputState('valid-input')
-    } else {
-      setPasswordInputState('default-input')
-    }
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    fetch(`/authenticate`, {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password }),
-    })
-      .then((response) => response.json())
-      .then((data => {
-        if (data.Errors !== undefined) {
-
-          setEmail(data.Data.email);
-          if (data.Errors.email !== undefined) {
-            setEmailErrorMsg(data.Errors.email[0]);
-            setPassword("")
-          }
-          if (data.Errors.password !== undefined) {
-            setPasswordErrorMsg(data.Errors.password[0]);
-            setPassword("")
-          }
+    const handleEmailChange = (e: string) => {
+        setEmail(e)
+        if (e === '') {
+            setEmailInputState('default-input')
+        } else {
+            setIsValid(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e));
+            if (isValid) {
+                setEmailInputState('valid-input')
+            } else {
+                setEmailInputState('invalid-input')
+            }
         }
-        if (data.access_token !== "") {
-          // get user to App.js
-          props.onUser({
-            firstName: data.first_name,
-            lastName: data.last_name,
-            accessToken: data.token.access_token,
-            accessLevel: data.access_level
-          })
+    };
+
+
+    const handlePasswordChange = (e: string) => {
+        setPassword(e)
+        if (e !== '') {
+            setPasswordInputState('valid-input')
+        } else {
+            setPasswordInputState('default-input')
         }
-      }))
-      .catch(err => {
-        console.log(err);
-      })
-  }
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        fetch(`/authenticate`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password }),
+        })
+            .then((response) => response.json())
+            .then((data => {
+                if (data.Errors !== undefined) {
+
+                    setEmail(data.Data.email);
+                    if (data.Errors.email !== undefined) {
+                        setEmailErrorMsg(data.Errors.email[0]);
+                        setPassword("")
+                    }
+                    if (data.Errors.password !== undefined) {
+                        setPasswordErrorMsg(data.Errors.password[0]);
+                        setPassword("")
+                    }
+                }
+                console.log(data)
+                if (data.access_token !== "") {
+                    // get user to App.js
+                    props.onUser({
+                        firstName: data.first_name,
+                        lastName: data.last_name,
+                        accessToken: data.token.access_token,
+                        accessLevel: data.access_level
+                    })
+                 
+                }
+            }))
+            .catch(err => {
+                console.log(err);
+            })
+    }
 
 
     const [openDialog, setOpenDialog] = React.useState(false);
@@ -103,7 +106,7 @@ export const LoginDialog = (props) => {
     }
 
     const openRegister = () => {
-      props.onOpenRegister()
+        props.onOpenRegister()
     }
 
     useEffect(() => {
@@ -114,63 +117,102 @@ export const LoginDialog = (props) => {
             open={openDialog}
             onClose={() => handleCloseDialog(false)}
             PaperProps={{
-             sx: { borderRadius: "10px", bgcolor: "-moz-initial" },
-             component: 'form',
-              onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-                handleSubmit(event)
-              },
+                sx: { borderRadius: "10px", bgcolor: "#bdbdbd" },
+                component: 'form',
+                onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+                    handleSubmit(event)
+                },
             }}>
-          <Box
-            sx={{
-              width: 300,
-              height: 400,
-              display: "flex",
-              justifyContent: "center",
-              flexDirection: "column",
-              alignItems: "center",
-              padding: 5,
-            }} >
-                <DialogTitle className="dialog-title">Login</DialogTitle>
-                <Input
-                    className={"default-input"}
-                    placeholder={"Email"}
-                    type={"email"}
-                    name={"email"}
-                    onChange={(e: any) => { handleEmailChange(e.target.value) }}
-                    errorMsg={emailErrorMsg}
-                />
-                <Input
-                    className={"default-input"}
-                    placeholder={"Password"}
-                    type={"password"}
-                    name={"password"}
-                    onChange={(e: any) => { handlePasswordChange(e.target.value) }}
-                    errorMsg={passwordErrorMsg}
-                />
-                
-                <DialogActions sx={{ display: "flex", flexDirection: "column" }}>
-                    <Button
-                        sx={{
-                            background: "#37474f",
-                            color: "#f5f5f5",
-                            width: 300,
-                            height: 50,
-                            borderRadius: 20,
-                            marginBottom: 2
-                        }}
-                        type="submit">Submit</Button>
-
-                    <Button
-                        variant="outlined"
-                        sx={{
-                            outline: "red",
+            <Box
+                sx={{
+                    width: 450,
+                    height: 500,
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                }} >
+                <Box
+                    sx={{
+                        width: "100%",
+                        height: "40%",
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                        alignItems: "center"
+                    }}>
+                    <span
+                        style={{
                             color: "#37474f",
-                            width: 300,
-                            height: 50,
-                            borderRadius: 20
+                            fontSize: 30,
+                            margin: 10,
                         }}
-                        onClick={openRegister}>Register</Button>
-                </DialogActions>
+                    >Welcome back</span>
+                    <span
+                        style={{
+                            color: "#37474f",
+                        }}
+                    >Glad to see you again.</span>
+                    <span
+                        style={{
+                            color: "#37474f",
+                        }}
+                    >Login to your account below</span>
+                </Box>
+                <Box sx={{
+                        width: "100%",
+                        height: "60%",
+                        display: "flex",
+                        justifyContent: "space-between",
+                        flexDirection: "column",
+                        background: "#e0e0e0",
+                    }} >  
+                <TextField
+                        required
+                        error={false}
+                        id="outlined-error-helper-text"
+                        helperText={""}
+                        label="enter email..."
+                        variant="outlined"
+                        type="email"
+                        sx={{ marginLeft: 4, marginRight: 4, marginTop: 3 }}
+                        onChange={(e: any) => { handleEmailChange(e.target.value) }}
+                    />
+                    <TextField
+                        required
+                        error={false}
+                        id="outlined-error-helper-text"
+                        helperText={""}
+                        label="enter password.."
+                        variant="outlined"
+                        type="password"
+                        sx={{ marginLeft: 4, marginRight: 4, marginTop: 3 }}
+                        onChange={(e: any) => { handlePasswordChange(e.target.value) }}
+                    />
+
+                    <Button sx={{
+                            marginLeft: 4,
+                            marginRight: 4,
+                            marginTop: 3,
+                            background: "#37474f",
+                            color: "#e0e0e0",
+                            borderRadius: 2,
+                            height: 50
+                        }}
+                        type="submit"
+                        onClick={handleSubmit}
+                    >Login</Button>
+                    <Box
+                        sx={{
+                            width: "100%",
+                            display: "flex",
+                            justifyContent: "center",
+                            marginBottom: 3,
+                            marginTop: 3,
+                        }} >
+                        <span style={{ color: "#37474f",paddingRight: 10 }} >Do not have an account?</span>
+                        <span style={{ color: "#01579b", cursor: "pointer" }}  onClick={openRegister}>Sign up for free</span>
+                    </Box>
+                </Box>
             </Box>
         </Dialog>
     )
