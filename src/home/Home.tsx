d727097956d15ff88e6bf5a7da8e37f8a2fdb4d6
Yuniesky Suarez <yuniesky@mobai.bio>
2025-01-26 18:22:55 +0100

    refactor home

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from "react"
 import { useOutletContext } from "react-router-dom";
 import "./home.scss"
 export const Home = () => {
-  const { employee } = useOutletContext();
+  const { userRole } = useOutletContext();
   const[welcome, setWelcome] = useState("Home")
   useEffect(() => {
-    if (employee.firstName !== "") {
-       setWelcome("Welcome " + employee.firstName + " " + employee.lastName)
-    }
-  }, [employee])
+   if (userRole.accessLevel == 2) {
+    setWelcome("Hello employee")
+   }
+   if (userRole.accessLevel == 1) {
+    setWelcome("Hello Admin")
+   }
+   if (userRole.accessLevel == 3) {
+    setWelcome("Hello client")
+   }
+  }, [userRole])
     return (
         <div className="home">
           {welcome}
