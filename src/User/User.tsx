5cfa8add133fc1702fd1a06ffee83e74ec5720e9
Yuniesky Suarez <yuniesky@mobai.bio>
2025-01-26 18:24:35 +0100

    add user page

--- src/User/User.tsx
+++ src/User/User.tsx
@@ -1,10 +1,44 @@
-import React from "react"
-import { useLocation } from "react-router-dom"
+import React, { useEffect, useState } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
 
 
 export const User = () => {
+   const navigate = useNavigate();
    const location = useLocation()
-   console.log(location.state.firstName)
+   const userId = location.state.id
+   const [userName, setUserName] = useState("");
+
+   useEffect(() => {
+      const handleUserLoading = async () => {
+        /*
+        try {
+            const res = await fetch("/user/profile", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ id: userId }),
+              });
+
+              if (!res.ok) {
+                const errorData = await res.json();
+                throw new Error(errorData.message || "Loading user failed");
+              }
+
+              const data = await res.json();
+              setUserName(data.first_name)
+              console.log("User Profile data", data)
+
+        }catch(error) {
+          console.log("Error loading user profile", error.message);
+          // Navigate to home and optionally trigger a failed alert
+          navigate("/", { state: { alert: "Error loading user profile. Please try again." } });
+        }
+        */
+      }
+      
+      handleUserLoading();
+   },[])
     return(
         <>
         User
